55c5cf626484d19f9d92760c3b987206b239f015
touilleWoman <jie.zhou.ac@gmail.com>
2023-10-04 16:44:51 +0200

    fix : day01 use generator, add part two

--- day01/solve.py
+++ day01/solve.py
@@ -1,40 +1,53 @@
-import itertools
+def elf_generator(lines):
+    elf =[]
+    for line in lines:
+        if line != '\n':
+            elf.append(int(line))
+        else:
+            yield sum(elf)
+            elf = []
 
-def main():
-    # with open('adventofcode.com_2022_day_1_input.txt', 'r') as f:
-    #     lines = f.readlines()
-    # new = []
-    # sum = 0
-    # for line in lines:
-    #     if line != '\n':
-    #         sum += int(line)
-    #     else:
-    #         new.append(sum)
-    #         sum = 0
-    # print(f"The answer is {max(new)}")
+
+def solve():
+    lines_gen = (line for line in open('adventofcode.com_2022_day_1_input.txt', 'r'))
+    elf_gen = elf_generator(lines_gen)
+
+    max = 0
+    for compared in elf_generator(lines_gen):
+        if compared > max:
+            max = compared
+    print(f"Biggest elf calories is {max}")
     
-    with open('adventofcode.com_2022_day_1_input.txt', 'r') as f:
-        max = 0
-        compared = 0
-        first_elf_summed = False
 
-        for index, line in enumerate(f):
-            if first_elf_summed is False:
-                if line !='\n':
-                    max += int(line)
-                else:
-                    first_elf_summed = True
-            else:
-                if line != '\n':
-                    compared += int(line)
-                else:
-                    if compared > max:
-                        max = compared
-                    compared = 0
-        
-    print(f"The answer is {max}")
+class TopThree:
+    """data structure stores the 3 biggest elfs when iterating lines
+    """
+    def __init__(self) -> None:
+        self.data =[0, 0, 0]
+    
+    def insert(self, compared):
+        self.data.append(compared)
+        self.data.sort(reverse=True)
+        self.data.pop()
+    
+    def sum(self):
+        return sum(self.data)
+    
+
+
+def solve_part_two():
+    lines_gen = (line for line in open('adventofcode.com_2022_day_1_input.txt', 'r'))
+    elf_gen = elf_generator(lines_gen)
+
+    top_three = TopThree()
+    
+    for compared in elf_generator(lines_gen):
+        top_three.insert(compared)
+    print(f"Total of three biggest elves calories is {top_three.sum()}")    
+       
         
 
 if __name__ == '__main__':
-    main()
+    solve()
+    solve_part_two()
     
