9ef1dd2147d8206d0e07efff6ff54cdbb18f2a32
touilleWoman <jie.zhou.ac@gmail.com>
2023-10-04 12:15:51 +0200

    add: day01 use iterable rather than list to solve

--- day01/solve.py
+++ day01/solve.py
@@ -1,19 +1,39 @@
 import itertools
 
 def main():
+    # with open('adventofcode.com_2022_day_1_input.txt', 'r') as f:
+    #     lines = f.readlines()
+    # new = []
+    # sum = 0
+    # for line in lines:
+    #     if line != '\n':
+    #         sum += int(line)
+    #     else:
+    #         new.append(sum)
+    #         sum = 0
+    # print(f"The answer is {max(new)}")
+    
     with open('adventofcode.com_2022_day_1_input.txt', 'r') as f:
-        lines = f.readlines()
-    new = []
-    sum = 0
-    for line in lines:
+        max = 0
+        compared = 0
+        first_elf_summed = False
+
+        for index, line in enumerate(f):
+            if first_elf_summed is False:
+                if line !='\n':
+                    max += int(line)
+                else:
+                    first_elf_summed = True
+            else:
+                if line != '\n':
+                    compared += int(line)
+                else:
+                    if compared > max:
+                        max = compared
+                    compared = 0
         
-        if line != '\n':
-            sum += int(line)
-        else:
-            new.append(sum)
-            sum = 0
+    print(f"The answer is {max}")
         
-    print(f"The answer is {max(new)}")
 
 if __name__ == '__main__':
     main()
