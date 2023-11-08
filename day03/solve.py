983bf2e4ca354b2755830b05593893376545d110
touilleWoman <jie.zhou.ac@gmail.com>
2023-11-08 21:35:44 +0100

    add day3 part two

--- day03/solve.py
+++ day03/solve.py
@@ -23,6 +23,23 @@ def solve():
         print(f"the sum of priorities is {sum}")
         return sum
 
+def common_item_value(group):
+    for x in group[0]:
+        if x in group[1] and x in group[2]:
+            return priority(x)
+    raise ValueError("There shold have common item in group")
+
+def solve_part_two():
+    with open(Path(__file__).with_name('adventofcode.com_2022_day_3_input.txt'), 'r') as fd:
+        line_gen = (line for line in fd)
+        sum = 0
+        while True:
+            group = [next(line_gen, None) for _ in range(3)]
+            if None in group:
+                break
+            sum += common_item_value(group)
+        print(f"Part Two: The sum of priorities is {sum}")
 
 if __name__ == "__main__":
     solve()
+    solve_part_two()
