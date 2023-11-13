16bba1a97ac002f37b8ad789e911a164b0ba3697
touilleWoman <jie.zhou.ac@gmail.com>
2023-11-13 11:43:35 +0100

    add: day 4 part two

--- day04/solve.py
+++ day04/solve.py
@@ -1,28 +1,35 @@
 from pathlib import Path
 import re
 
-def fully_contains(line) -> bool:
-    sections = re.split(r'[,-]', line)
-    sections = [int(x) for x in sections]
+def fully_contains(sections) -> bool:
     if sections[0] <= sections[2] and sections[1] >= sections[3]:
-        print(sections)
-        
         return True
     elif sections[0] >= sections[2] and sections[1] <= sections[3]:
-        print(sections)
-        
         return True
     else:
         return False
     
-
+def overlap(sections) -> bool:
+    pair_one = [x for x in range(sections[0], sections[1] + 1)]
+    pair_two = [x for x in range(sections[2], sections[3] + 1)]
+    for x in pair_one:
+        if x in pair_two:
+            return True
+    return False
+    
 def solve():
     with open(Path(__file__).with_name('adventofcode.com_2022_day_4_input.txt'), 'r') as fd:
         counter = 0
+        counter_part_two = 0
         for line in fd:
-            if fully_contains(line):
+            sections = re.split(r'[,-]', line)
+            sections = [int(x) for x in sections]
+            if fully_contains(sections):
                 counter += 1
+            if overlap(sections):
+                counter_part_two += 1
         print(f"There are {counter} paires where one fully contains the other")
+        print(f"There are {counter_part_two} paires with range overlap")
                     
 if __name__ == "__main__":
     solve()
